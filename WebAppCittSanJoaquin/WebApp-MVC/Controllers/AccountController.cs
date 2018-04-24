0b45a1878a4b2e5f0c500c6ba8f53c1c6547241e
nobodynuf <jo.manriqueza@alumnos.duoc.cl>
2018-04-24 02:37:46 -0300

    Agregados algunos ejemplos de acciones que se pueden realizar en MVC, y unos links de referencia en Index.html

---
+++ WebAppCittSanJoaquin/WebApp-MVC/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // El index se accesa cuando no se
+        // tiene ningun parametro cuando se accede al controlador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Este metodo procesa el formulario
+        // luego de eso, redirecciona a otra pagina
+        #warning Notese que el parametro es de tipo FormModel
+        [HttpPost]
+        public ActionResult FormLogin(Models.FormModel formData)
+        {
+            // usamos los datos del form, en este caso, textBoxData de FormModel.
+            return RedirectToAction("Exito", new { parametro = formData.TextBoxStringData });
+        }
+
+        // Este metodo recibe el parametro "parametro"
+        // comprueba que no este vacio, y luego,
+        // se lo pasa a la vista
+#warning Notese que el parametro que se entrega tiene el mismo nombre que la clase anonima pasada por metodo RedirectToAction
+
+        public ActionResult Exito(string parametro)
+        {
+            // Si no esta vacia, entonces pasamos el parametro a la vista
+            if (!string.IsNullOrEmpty(parametro))
+            {
+                ViewBag.Resultado = parametro;
+            }
+            return View();
+        }
+    }
+}
---
+++ WebAppCittSanJoaquin/WebApp-MVC/Models/FormModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Recursos;
+namespace WebApp_MVC.Models
+{
+    public class FormModel
+    {
+        #warning Aqui se usa un archivo de recursos!!
+        /** 
+        * Asignamos el label por defecto de cada propiedad.
+        * ademas, tenemos que dejar definido que archivo de
+        * recurso usamos. 
+        * Los archivos de recursos, en codigo, son clases estaticas,
+        * por eso no hay ni una instanciación
+        **/
+        [Required]
+        [Display(Name = "Campo1Label", ResourceType = typeof(Recursos.Resources))]
+        public string TextBoxStringData { get; set; }
+
+        [Required]
+        [Display(Name = "Campo2Label", ResourceType = typeof(Recursos.Resources))]
+        public int TextBoxIntData { get; set; }
+
+        [Required]
+        [Display(Name = "Campo3Label", ResourceType = typeof(Recursos.Resources))]
+        public bool CheckBoxData { get; set; }
+    }
+}
