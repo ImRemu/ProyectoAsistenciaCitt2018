9e509ec4d591dc10a9bbc4c4a850d8ef17f444eb
Rheinded/Vige <vige_cl_gb_96@hotmail.cl>
2018-04-28 21:16:39 -0300

    se genera el registro del usuario y ademas el log de accion de la creacion del usuario, con sus debidos mensajes de error y confirmacion

--- WebAppCittSanJoaquin/WebApp-MVC/Controllers/AccountController.cs
+++ WebAppCittSanJoaquin/WebApp-MVC/Controllers/AccountController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp_MVC.Models;
 
 namespace WebApp_MVC.Controllers
 {
     public class AccountController : Controller
     {
+        satcEntities dtb = new satcEntities();
+
         // El index se accesa cuando no se
         // tiene ningun parametro cuando se accede al controlador
         public ActionResult Index()
@@ -45,8 +48,39 @@ namespace WebApp_MVC.Controllers
             return View();
         }
 
-        public ActionResult Registrarse()
+        public ActionResult Registrarse(usuario user)
         {
+            log_acciones log = new log_acciones();
+            if(!string.IsNullOrEmpty(user.nombre) && !string.IsNullOrEmpty(user.apellidos) &&
+               !string.IsNullOrEmpty(user.correo) && !string.IsNullOrEmpty(user.password))
+            {
+                if(dtb.usuario.FirstOrDefault(u => u.correo.Equals(user.correo)) != null)
+                {
+                    TempData["error"] = "El email ya esta en uso";
+                    ViewBag.Error = TempData["error"];
+                    return View("Registrarse");
+                }
+                else
+                {
+                    //se genera un log de accion
+                    log.fecha = Convert.ToDateTime(DateTime.Now.ToString("dd-MM-yy hh:mm:ss"));
+                    log.accion = "Creacion de Usuario Alumno";
+                    log.nombre_ejecutor = "Usuario Nuevo";
+                    log.id_ejecutor = 0;
+                    //se le da la habilitacion, 0 = no habilitado, y el tipo usuario.
+                    user.habilitado = 0;
+                    user.tipo_usuario = "a";
+                    //se guardan los cambios en la base de datos.
+                    dtb.usuario.Add(user);
+                    dtb.log_acciones.Add(log);
+                    dtb.SaveChanges();
+                    //se retorna el mensaje correspondiente.
+                    TempData["creado"] = "El Usuario se ha registrado exitosamente.";
+                    ViewBag.Registrado = TempData["creado"];
+                    return View("Exito");
+                }
+
+            }
             return View();
         }
     }
