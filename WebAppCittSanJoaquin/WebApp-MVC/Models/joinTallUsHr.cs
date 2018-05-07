b257ff32e951d75b1b1c64e391bc3bfe10ba91e1
Rheinded/Vige <vige_cl_gb_96@hotmail.cl>
2018-05-07 19:58:43 -0300

    se modifico el mostreo de los talleres ahora en vez de solo mostrar solo los datos del taller, mostrara los datos del taller, su encargado y horario respectivo.

---
+++ WebAppCittSanJoaquin/WebApp-MVC/Models/joinTallUsHr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_MVC.Models
+{
+    public class joinTallUsHr
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public int id_taller { get; set; }
+        public string nombreTaller { get; set; }
+        public string descripcion { get; set; }
+        public System.DateTime hora_inicio { get; set; }
+        public System.DateTime hora_termino { get; set; }
+    }
+}
