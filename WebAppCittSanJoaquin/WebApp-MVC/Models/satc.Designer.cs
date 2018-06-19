cbc9cd0467ac22990677b255cacb2515395f15a7
Jorge R <jorge3ctp@hotmail.com>
2018-06-19 15:40:19 -0400

    Cambios en el diagrama de bd y cosas raras que hizo don Jorge Manriquez

--- WebAppCittSanJoaquin/WebApp-MVC/Models/satc.Designer.cs
+++ WebAppCittSanJoaquin/WebApp-MVC/Models/satc.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\nobodynuf\source\repos\SATC\WebAppCittSanJoaquin\WebApp-MVC\Models\satc.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jorge\Source\Repos\ProyectoAsistenciaCitt2018\WebAppCittSanJoaquin\WebApp-MVC\Models\satc.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
